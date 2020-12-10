44416c611cd7b1aef31b61b7eb81166a96dd740a
hooriahic <hooria.hic@gmail.com>
2020-12-10 20:06:47 +0500

    added loader to main pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import React from 'react';
-import Home from "./components/pages/home/home";
-import About from "./components/pages/about/about";
-import Contact from "./components/pages/contact/contact";
-import Projects from "./components/pages/projects/projects";
-import Error from "./components/pages/error/error";
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,38 +6,50 @@ import {
 } from "react-router-dom";
 import './App.css';
 // import AnimatedCursor from "react-animated-cursor";
+const Home = React.lazy(() => import("./components/pages/home/home"));
+const About =  React.lazy(() => import("./components/pages/about/about"));
+const Projects =  React.lazy(() => import("./components/pages/projects/projects"));
+const Error = React.lazy(() => import("./components/pages/error/error"));
+const Contact =  React.lazy(() => import("./components/pages/contact/contact"));
+
 
 function App() {
   return (
       <Router>
-        <div>
-        {/* <AnimatedCursor
-          innerSize={8}
-          outerSize={20}
-          color='183, 0, 255'
-          outerAlpha={0.2}
-          innerScale={1}
-          outerScale={2}
-        /> */}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-            <Route path="/projects">
-              <Projects />
-            </Route>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            
-            <Route>
-              <Error />
-            </Route>
-          </Switch>
-        </div>
+        <Suspense fallback={
+          <div className="loadingScreen">
+            <div className="loader"></div>
+          </div>
+        }>
+          <div>
+          {/* <AnimatedCursor
+            innerSize={8}
+            outerSize={20}
+            color='183, 0, 255'
+            outerAlpha={0.2}
+            innerScale={1}
+            outerScale={2}
+          /> */}
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/contact">
+                <Contact />
+              </Route>
+              <Route path="/projects">
+                <Projects />
+              </Route>
+              <Route path="/" exact>
+                  <Home />
+              </Route>
+              
+              <Route>
+                <Error />
+              </Route>
+            </Switch>
+          </div>
+        </Suspense>
       </Router>
   );
 }
